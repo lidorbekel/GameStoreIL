83da8e733ef98df2d79cb6c05907f79485f38fcf
lidor bekel <lidor.bekel@camilyo.com>
2021-09-09 18:26:48 +0300

    init startup.cs

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace WebApplication_GameStoreIL
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime, Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,7 +44,7 @@ namespace WebApplication_GameStoreIL
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days, You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
